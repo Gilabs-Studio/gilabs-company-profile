ec91bd5b452c2d625a4b1538dca3ad27853e8758
Kevin5621 <yohanes123kevin123@gmail.com>
2025-11-26 11:11:03 +0700

    feat: initialize dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,32 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
+import robotsTxt from 'astro-robots-txt';
+import compress from 'astro-compress';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    react(),
+    sitemap(),
+    robotsTxt(),
+    compress()
+  ],
+
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en", "id"],
+    routing: {
+      prefixDefaultLocale: false
+    }
+  },
+  
+  site: 'https://gilabs.id'
+});
